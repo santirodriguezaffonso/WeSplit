5fa644deb40f19fd4c21cc0e1bbf02414dd3676e
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-05-15 20:00:26 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Santiago Rodriguez Affonso on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
